dc43f617e7cb6943c2fb7afaad0df32a58957948
Binit Koirala <binitkoirala8@gmail.com>
2024-07-22 21:34:18 +0545

    Message.

--- module_2/hard/solution_15.py
+++ module_2/hard/solution_15.py
@@ -3,12 +3,12 @@
 transportation_mode = input("Enter a mode of transportation (car, bus, bike, walk): ").lower()
 match transportation_mode:
     case "car":
-        print("The average speed of a car is 60 mph.")
+        print("The average speed of a car is 45 mph.")
     case "bus":
         print("The average speed of a bus is 40 mph.")
     case "bike":
-        print("The average speed of a bike is 15 mph.")
+        print("The average speed of a bike is 10 mph.")
     case "walk":
-        print("The average speed of walking is 3 mph.")
+        print("The average speed of walking is 2 mph.")
     case _:
         print("Invalid mode of transportation entered. Please enter a valid mode (car, bus, bike, walk).")
