decced8f5e739f64a718a744a43f1e3a8a600288
Binit Koirala <binitkoirala8@gmail.com>
2024-07-22 19:52:09 +0545

    Message.

--- module_1/hard/solution_27.py
+++ module_1/hard/solution_27.py
@@ -1,17 +1,4 @@
-# Write a program that prints all prime numbers between 1 and 100.
+# # Write a program that prints all prime numbers between 1 and 100.
 
-# prime numbers are number which is only divisible by itself or 1.
-
-list = []
-
-numbers = int(input("Enter the Highest range of prime numbers: "))
-
-for i in range (numbers):
-    i += 1
+for i in range (5):
     print (i)
-    continue
-
-for j in range (numbers):
-    
-    
-    
